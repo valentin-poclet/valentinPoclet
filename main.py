2025744e50d5b1060b5d7393ad4d543183a0cffe
valentin-poclet <valentin.poclet@edu.esiee.fr>
2024-11-15 14:48:03 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
+"""
+import essentiel
+"""
 #### Fonction secondaire
+import string
+from unidecode import unidecode
 
 
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """
+    Fonction permettant de verifier si le mot p est un palindrome
+    """
+    p = unidecode(p).translate(str.maketrans('','',string.punctuation)).upper().replace(" ","")
+    if len(p) == 1:
+        return True
+    if p == "":
+        return True
+    if p[0] == p[-1]:
+        return ispalindrome(p[1:-1])
     return False
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    fonction main
+    """
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
